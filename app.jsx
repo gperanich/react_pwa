f53f0e71b27de1a69ea52cdbbf415c9780dda530
gperanich <gperanich@gmail.com>
2017-01-20 14:14:03 -0500

    first commit

---
+++ app.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        var myStyle = {
+            color: '#FF0000'
+        }
+
+        return (
+            <div>
+                <Header/>
+                <Content/>
+                <p>{1 + 1}</p>
+            </div>
+        );
+
+    }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
+         </div>
+      );
+   }
+}
+
+
+export default App;
