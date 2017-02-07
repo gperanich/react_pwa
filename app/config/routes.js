1deb54e9ae1b25d030fea8cbe420cee7445f08b4
Peranich <ZK30JJV@corp.bankofamerica.com>
2017-02-07 16:52:50 -0500

    added stateless components

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,13 +7,17 @@ var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
 var PromptContainer = require('../containers/PromptContainer');
+var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
+var ResultsContainer = require('../containers/ResultsContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
-      <Route path='playerOne' component={PromptContainer}/>
-      <Route path='playerTwo/:playerOne' component={PromptContainer} />
+      <Route path='playerOne' header='Player One' component={PromptContainer}/>
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
+      <Route path='battle' component={ConfirmBattleContainer} />
+      <Route path='results' component={ResultsContainer} />
     </Route>
   </Router>
 );
